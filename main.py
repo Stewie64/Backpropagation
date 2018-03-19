45e4c9a48718251257f3f952de13d1f83ba93f36
root <root@localhost.localdomain>
2018-03-19 17:08:49 -0500

    ErrorCalculate

--- main.py
+++ main.py
@@ -2,38 +2,51 @@ import math
 
 
 #Constants
-entries = [[0, 0], [1, 0], [0, 1], [1, 1]]
-weightsIn = [[0.1, 0.5], [-0.7, 0.3]]
-weightsOut = [0.2, 0.4]
-learnRat = 0.25
-e = math.e
-outH_Array = []
+
+
 
 
 class backpropagation:
     def __init__(self):
         self.netVal = 0
+        self.entries = [[0, 0], [1, 0], [0, 1], [1, 1]]
+        self.expectedValOut=[0,1,1,0]
+        self.expectedValIndex=0
+        self.weightsIn = [[0.1, 0.5], [-0.7, 0.3]]
+        self.weightsOut = [0.2, 0.4]
+        self.hiddenLayerOuts = []
+        self.outputLayerOut=0
+        self.error = 0
+        self.learnRat = 0.25
+        self.e = math.e
 
     #Calculate the first NET value
     def netHidden(self):
         index = 0
-        for eTemp in entries:
-            for e in eTemp:
-                for w in weightsIn:
-                    self.netVal += w[index] * e
+        for eTemp in self.entries:
+            for ent in eTemp:
+                for w in self.weightsIn:
+                    self.netVal += w[index] * ent
                 index += 1
-                outH_Array.append(self.outFunction(self.netVal))
-
+                self.hiddenLayerOuts.append(self.outFunction(self.netVal))
+            netOut()
+            self.expectedValIndex+=1
     #return out function value for each hidden neuron
     def outFunction(self,value):
-        return 1 / (1 + (math.pow(e,value)))
+        return 1 / (1 + (math.pow(self.e,value)))
 
 
-    #Calculates the NET value in the hidden layer
+    #Calculates the NET value in the out layer
     def netOut(self):
         flag = 0
         netOutVal = 0
-        for tempEntry in outH_Array:
-            netOutVal += weightsOut[flag] * tempEntry
+        for tempEntry in hiddenLayerOuts:
+            netOutVal += self.weightsOut[flag] * tempEntry
             flag += 1
-            return self.outFunction(netOutVal)
+        self.outputLayerOut = self.outFunction(netOutVal)
+
+
+    def calculateError(self):
+        self.error=self.expectedValOut[self.expectedValIndex]- self.outputLayerOut
+    def deltaFunction(self):
+
